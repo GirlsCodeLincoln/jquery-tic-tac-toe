a583917e6837b3f696ff43bb1d87baad1fb8c359
troynguyen8@gmail.com <troynguyen8@gmail.com>
2018-11-11 12:45:51 -0600

    added more logic

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-  console.log("hello world");
+  var move = 1;
+  var play = true;
+  var player = "1";
+
+  $(".game-space").click(function () {
+    // debugger;
+    var $squareClicked = $(this);
+    if ($squareClicked.text() === "" && play) {
+      if (player === "1") {
+        $squareClicked.append("X");
+        player = "2";
+      } else {
+        $squareClicked.append("O");
+        player = "1";
+      }
+
+      move = move + 1;
+    }
+  });
 });
